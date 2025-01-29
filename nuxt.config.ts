8c8ffd273f920bba667f19098a9cc14081e2fa59
Pooya Parsa <pooya@pi0.io>
2025-01-29 19:50:20 +0100

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-29',
-  ssr: true,
+  ssr: false,
   nitro: {
     errorHandler: 'nitropack/dist/runtime/error',
-    //   externals: {
-    //     traceInclude: ['vue/server-renderer'],
-    //   },
   },
 });
