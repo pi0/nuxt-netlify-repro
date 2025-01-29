dff2533e23237a762c6b8759b62e7378e8d56d39
Pooya Parsa <pooya@pi0.io>
2025-01-29 20:10:11 +0100

    better stack

--- error-handler.mjs
+++ error-handler.mjs
@@ -5,6 +5,6 @@ export default function defaultNitroErrorHandler(error, event) {
   console.error(error);
   return send(event, JSON.stringify({
     error: error.message,
-    stack: error.stack,
+    stack: error.stack.split("\n").map((line) => line.trim()),
   }, null, 2));
 }
